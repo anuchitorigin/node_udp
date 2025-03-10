d245f0504e61088722a179c4464db940b7efc276
Anuchit Butkhunthong <anuchit.b@origin55.com>
2025-03-10 14:51:00 +0700

    2nd commit

--- src/server.ts
+++ src/server.ts
@@ -24,3 +24,27 @@ server.on('listening', () => {
 
 server.bind(55062);
 // Prints: server listening 0.0.0.0:55062
+
+// const loop = () => {
+//   const now = new Date();
+//   console.log(`[${now.toLocaleString()}] client is running...`);
+//   // creating a client socket
+//   var client = dgram.createSocket('udp4');
+
+//   //buffer msg
+//   // var data = Buffer.from('SI\r\n');
+//   const data = 'SI\r\n';
+
+//   //sending msg
+//   client.send([data], 55061, '192.168.1.250', function(error) {
+//     if(error){
+//       client.close();
+//     }else{
+//       console.log('Data sent !!!');
+//     }
+//   });
+//   setTimeout(loop, 5000);
+// }
+
+// loop();
+
